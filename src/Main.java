4c185a960258d5fca2b1a748fca5b8cfdc47c672
jannakam <jannakalmuqaisib@gmail.com>
2024-11-06 21:08:31 +0300

    try catch task done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        String validUsername = "Coded";
+        String validPassword = "Coded123";
+        boolean isLoggedIn = false;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // FIRST BONUS
+//        do {
+//            try {
+//                System.out.println("Enter a username: ");
+//                String inputUser = scanner.nextLine();
+//                if (!inputUser.equals(validUsername)) {
+//                    throw new Exception("Invalid credentials");
+//                }
+//
+//                System.out.println("Enter a password: ");
+//                String inputPass = scanner.nextLine();
+//                if (!inputPass.equals(validPassword)) {
+//                    throw new Exception("Invalid credentials");
+//                }
+//
+//                isLoggedIn = true;
+//                System.out.println("Logged in successfully");
+//            }
+//            catch (Exception e) {
+//                System.out.println(e.getMessage());
+//            }
+//        } while (!isLoggedIn);
+
+        // EXTRA BONUS
+        for (int i = 0; i < 6; i++) {
+            try {
+                if (i == 5) {
+                    throw new Exception("Exceeded maximum number of attempts. Try again later");
+                }
+                System.out.println("Enter a username: ");
+                String inputUser = scanner.nextLine();
+                if (!inputUser.equals(validUsername)) {
+                    throw new Exception("Invalid credentials");
+                }
+
+                System.out.println("Enter a password: ");
+                String inputPass = scanner.nextLine();
+                if (!inputPass.equals(validPassword)) {
+                    throw new Exception("Invalid credentials");
+                }
+                isLoggedIn = true;
+                System.out.println("Logged in successfully");
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
+
+        scanner.close();
     }
 }
